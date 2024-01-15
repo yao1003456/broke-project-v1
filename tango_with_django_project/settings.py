1a06c1e060b6229b03de56563049cdab28dda794
Zhen Yao Heng <2840506h@student.gla.ac.uk>
2024-01-15 21:38:17 +0000

    c4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = '90ixmsaiy-@9c!ic9arkekgtj99jr2__d@-f=(y#=30uvp#9*9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [TEMPLATE_DIR, ]
 
 
 # Application definition
