37924ed3efbb561f710a7d783aa8dcc456ff9d27
Zhen Yao Heng <2840506h@student.gla.ac.uk>
2024-01-18 10:48:19 +0000

    c4

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,5 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcakes!'}
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    return HttpResponse("Rango says here is the about page" + "<a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'yao'}
+    return render(request, 'rango/about.html', context=context_dict)
