fcf2f7cfecb26a20dcd80d3e5e158a7d37fbc09b
Zhen Yao Heng <2840506h@student.gla.ac.uk>
2024-01-22 23:24:13 +0000

    C3 final

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner!" + "<a href='/rango/about'>About</a>")
+
 def about(request):
-    context_dict = {'boldmessage': 'yao'}
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page." + "<a href='/rango/'>Index</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,9 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +55,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,7 +63,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
             ],
         },
     },
@@ -121,8 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = '/static/'
 
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
