842508f40a09e7697bb93558bce0e1b0a369cbbc
Zhen Yao Heng <2840506h@student.gla.ac.uk>
2024-01-22 10:42:08 +0000

    C4

--- rango/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rango import views
-
-app_name = 'rango'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path("about/", views.about, name="about")
-]
--- rango/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcakes!'}
-    return render(request, 'rango/index.html', context=context_dict)
-def about(request):
-    context_dict = {'boldmessage': 'yao'}
-    return render(request, 'rango/about.html', context=context_dict)
